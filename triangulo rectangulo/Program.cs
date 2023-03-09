6d91fa566d3fe86152df587b5e5740f8337118a7
zamu134 <117544767+zamu134@users.noreply.github.com>
2023-03-09 12:18:11 -0600

    Add files via upload

---
+++ triangulo rectangulo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Introduce un número entero: ");
+        int altura = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= altura; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
